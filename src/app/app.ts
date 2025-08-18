529232866503d4d44fea237bb8bca5cebba50820
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-08-18 18:37:32 -0500

    fix: added 'withFetch' in HTTP client to view 'poke-list' component in main module

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { List } from './list/list/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, List],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
