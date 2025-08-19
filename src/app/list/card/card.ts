9916c7d85b3c6f8cdf862e59c59459c250eb7b9e
samuelbonifacio015 <samuelbonifacio015@gmail.com>
2025-08-19 10:47:50 -0500

    feat: added img & card styles to pokemon

---
+++ src/app/list/card/card.ts
@@ -0,0 +1,24 @@
+import { Component, input } from '@angular/core';
+import { PokeResult } from '../../core/models/poke-result.model';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [],
+  template: `
+  <div class="poke-card">
+    <img
+      width="120"
+      height="120"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      [alt]="pokeResult()?.name || 'Pokémon'"
+    />
+    <p>{{ pokeResult()?.name || 'Sin nombre' }}</p>
+    <small>{{ pokeResult()?.url || 'Sin URL' }}</small>
+  </div>
+  `,
+  styleUrl: './card.css'
+})
+export class Card {
+  readonly pokeResult = input.required<PokeResult>();
+}
--- src/app/list/list/list.ts
+++ src/app/list/list/list.ts
@@ -1,28 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { PokeService } from '../../core/services/poke.service';
 import { CommonModule } from '@angular/common';
+import { Card } from '../card/card';
 
 @Component({
   selector: 'poke-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, Card],
   template: `
     <h1 class="title">My Pokémon List</h1>
     <div class="poke-container">
       @if (pokeList$ | async; as pokeList) {
         <div class="poke-cards">
+          <p>Encontrados {{ pokeList.results?.length || 0 }} Pokémon</p>
           @for (poke of pokeList.results; track poke.name) {
-            <div class="poke-card">
-              <h3>{{ poke.name }}</h3>
-              <p>{{ poke.url }}</p>
-            </div>
+            <app-card [pokeResult]="poke" />
           }
         </div>
       } @else {
         <p>Loading...</p>
       }
     </div>
-  `,
+  `,  
   styleUrl: './list.css',
 })
 export class List {
